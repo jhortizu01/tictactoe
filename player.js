3bdddafd83301c76839c225a23cf1bf258d5cf04
Janika Hortizuela <jhortizuela21@gmail.com>
2021-06-15 09:43:33 -0600

    Refactor player.js

--- player.js
+++ player.js
@@ -5,23 +5,21 @@ class Player {
     this.wins = 0;
   }
 
-
   saveWinsToStorage() {
   if (this.playerNumber === 'one') {
     localStorage.setItem('player1 wins', JSON.stringify(game.playerOne.wins))
   } else if (this.playerNumber === 'two') {
     localStorage.setItem('player2 wins', JSON.stringify(game.playerTwo.wins))
-  }
-}
-
-retrieveWinsFromStorage() {
-   if (this.playerNumber === 'one') {
-     playerOne.wins = (JSON.parse(localStorage.getItem('player1 wins')) || 0)
-     this.wins = playerOne.wins
-   } else if (this.playerNumber === 'two') {
-     playerTwo.wins = (JSON.parse(localStorage.getItem('player2 wins')) || 0)
-     this.wins = playerTwo.wins
    }
  }
 
-}
+  retrieveWinsFromStorage() {
+     if (this.playerNumber === 'one') {
+       playerOne.wins = (JSON.parse(localStorage.getItem('player1 wins')) || 0)
+       this.wins = playerOne.wins
+     } else if (this.playerNumber === 'two') {
+       playerTwo.wins = (JSON.parse(localStorage.getItem('player2 wins')) || 0)
+       this.wins = playerTwo.wins
+      }
+    }
+  }
